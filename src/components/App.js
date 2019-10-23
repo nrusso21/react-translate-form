4e677446c42b8b0ab2ae7212d4e0d0b9f104260e
Nicholas Russo <nrusso@protonmail.com>
2019-10-23 13:01:36 -0400

    React context translate project

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LanguageSelector from './LanguageSelector'
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext'
+
+class App extends React.Component {
+    render() {
+        return(
+            <div className="ui container" >
+                <LanguageStore>
+                    <LanguageSelector/>
+                    <ColorContext.Provider value="red">
+                        <UserCreate/>
+                    </ColorContext.Provider>
+                </LanguageStore>
+            </div>
+        );
+    }
+}
+
+export default App;
